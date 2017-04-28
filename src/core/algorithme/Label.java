bd1bb21b9ce298919b0bff6da41656deea58d642
anaisrabary <anais.rabary@gmail.com>
2017-04-28 15:15:57 +0200

    label modifié getteur et setteurs

--- src/core/algorithme/Label.java
+++ src/core/algorithme/Label.java
@@ -8,12 +8,12 @@ public class Label {
 	private Noeud pere;
 	private Noeud sommetCourant;
 	
-	public Label(Noeud sommetCourant, boolean marquage)	{
+	public Label(Noeud sommetCourant)	{
 		if(sommetCourant == null)
 			throw new IllegalArgumentException("sommet courant est null\n");
 		
 		this.sommetCourant = sommetCourant;
-		this.marquage = marquage;
+		this.marquage = false;
 	}
 	
 	/**
@@ -22,5 +22,29 @@ public class Label {
 	 */
 	public boolean isMarque()	{
 		return marquage;
+	}
+	
+	/**
+	 * getteur et setteurs
+	 */
+	
+	public void setMarquage(boolean marq) {
+		this.marquage = marq ;
+	}
+	
+	public void setPere(Noeud papa){
+		this.pere = papa ;
+	}
+	public void setCout(float cout){
+		this.cout = cout ;
+	}
+	public Noeud getPere (){
+		return this.pere ;
+	}
+	public boolean getMarquage(){
+		return this.marquage ;
+	}
+	public float getCout(){
+		return this.cout ;
 	}
 }
